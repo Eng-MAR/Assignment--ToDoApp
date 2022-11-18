e62b39180e8fc487739aa7767311f5e53f48d696
Eng-MAR <Drrahman911@hotmail.com>
2022-11-17 20:44:56 -0500

    Initial Commit: Defining the model

---
+++ ToDo App/Models/ToDoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo_App.Models
+{
+    public class ToDoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDone { get; set; }
+
+    }
+}
